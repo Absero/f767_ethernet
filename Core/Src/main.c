54b982f54054b44984c2576306c6029566567f04
Mantas A <mantas.ambraziunas@gmail.com>
2021-01-21 15:01:33 +0200

    UDP siuntimas
    Gale disconnect ir remove gali but nereikalingos, bet nustoja siust tiesiog be ju

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include "udp.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -313,11 +314,30 @@ void StartDefaultTask(void const * argument)
   /* init code for LWIP */
   MX_LWIP_Init();
   /* USER CODE BEGIN 5 */
+
+  const char* message = "Hello world!\n\r";
+
   /* Infinite loop */
   for(;;)
   {
     osDelay(1000);
     HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+
+    ip_addr_t PC_IPADDR;
+    IP_ADDR4(&PC_IPADDR, 192, 168, 1, 107);
+
+    struct udp_pcb* my_udp = udp_new();
+    udp_connect(my_udp, &PC_IPADDR, 55151);
+    struct pbuf* udp_buffer = NULL;
+
+    udp_buffer = pbuf_alloc(PBUF_TRANSPORT, strlen(message), PBUF_RAM);
+    if (udp_buffer != NULL) {
+      memcpy(udp_buffer->payload, message, strlen(message));
+      udp_send(my_udp, udp_buffer);
+      pbuf_free(udp_buffer);
+    }
+    udp_disconnect(my_udp);
+    udp_remove(my_udp);
   }
   /* USER CODE END 5 */
 }
