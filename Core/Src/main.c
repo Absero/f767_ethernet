ff4e3c717f39dde647c6a085b764fd3a4c1ba9fd
Mantas A <mantas.ambraziunas@gmail.com>
2021-01-25 17:29:15 +0200

    http serverio biblioteka
    Viskas perkelta i biblioteka

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,10 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <string.h>
-#include "udp.h"
-#include "lwip/api.h"
-#include "../../webpages/index.h"
+#include "html_webpage.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -61,12 +58,7 @@ static void MX_USART3_UART_Init(void);
 void StartDefaultTask(void const *argument);
 
 /* USER CODE BEGIN PFP */
-void udp_receive_callback(void *arg, struct udp_pcb *upcb, struct pbuf *p,
-		const ip_addr_t *addr, u16_t port);
 void ethernetif_notify_conn_changed(struct netif *netif);
-void http_server_netconn_init();
-static void http_server_netconn_thread(void *arg);
-void http_server_serve(struct netconn *conn);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -298,96 +290,6 @@ static void MX_GPIO_Init(void) {
 }
 
 /* USER CODE BEGIN 4 */
-void http_server_netconn_init() {
-	sys_thread_new("HTTP", http_server_netconn_thread, NULL,
-	DEFAULT_THREAD_STACKSIZE, WEBSERVER_THREAD_PRIO);
-}
-
-static void http_server_netconn_thread(void *arg) {
-	struct netconn *conn, *newconn;
-	err_t err, accept_err;
-
-	/* Create a new TCP connection handle */
-	conn = netconn_new(NETCONN_TCP);
-
-	if (conn != NULL) {
-		/* Bind to port 80 (HTTP) with default IP address */
-		err = netconn_bind(conn, NULL, 80);
-
-		if (err == ERR_OK) {
-			/* Put the connection into LISTEN state */
-			netconn_listen(conn);
-
-			while (1) {
-				/* accept any icoming connection */
-				accept_err = netconn_accept(conn, &newconn);
-				if (accept_err == ERR_OK) {
-					/* serve connection */
-					http_server_serve(newconn);
-
-					/* delete connection */
-					netconn_delete(newconn);
-				}
-			}
-		}
-	}
-}
-
-void http_server_serve(struct netconn *conn) {
-	struct netbuf *inbuf;
-	err_t recv_err;
-	char *buf;
-	u16_t buflen;
-
-	/* Read the data from the port, blocking if nothing yet there.
-	 We assume the request (the part we care about) is in one netbuf */
-	recv_err = netconn_recv(conn, &inbuf);
-
-	if (recv_err == ERR_OK) {
-		if (netconn_err(conn) == ERR_OK) {
-			netbuf_data(inbuf, (void**) &buf, &buflen);
-
-			/* Is this an HTTP GET command? (only check the first 5 chars, since
-			 there are other formats for GET, and we're keeping it very simple )*/
-			if ((buflen >= 5) && (strncmp(buf, "GET /", 5) == 0)) {
-				if (strncmp((char const*) buf, "GET /index.html", 15) == 0) {
-					netconn_write(conn, (const unsigned char* )index_html,
-							index_html_len, NETCONN_NOCOPY);
-				}
-//				if (strncmp((char const*) buf, "GET /led1", 9) == 0) {
-//					HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-//				}
-//				if (strncmp((char const*) buf, "GET /led2", 9) == 0) {
-//					HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-//				}
-//				if (strncmp((char const*) buf, "GET /led3", 9) == 0) {
-//					HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-//				}
-//				if (strncmp((char const*) buf, "GET /btn1", 9) == 0) {
-//					if (HAL_GPIO_ReadPin(User_Blue_Button_GPIO_Port,
-//							User_Blue_Button_Pin) == GPIO_PIN_SET)
-//						netconn_write(conn, (const unsigned char* )"ON", 2,
-//								NETCONN_NOCOPY);
-//					else
-//						netconn_write(conn, (const unsigned char* )"OFF", 3,
-//								NETCONN_NOCOPY);
-//				}
-//				if (strncmp((char const*) buf, "GET /adc", 8) == 0) {
-//					sprintf(buf, "%2.1f °C", getMCUTemperature());
-//					netconn_write(conn, (const unsigned char* )buf, strlen(buf),
-//							NETCONN_NOCOPY);
-//				}
-			}
-		}
-	}
-	/* Close the connection (server closes in HTTP) */
-	netconn_close(conn);
-
-	/* Delete the buffer (netconn_recv gives us ownership,
-	 so we have to make sure to deallocate the buffer) */
-	netbuf_delete(inbuf);
-}
-
 uint8_t statusChanged = 0;
 void ethernetif_notify_conn_changed(struct netif *netif) {
 	/* NOTE : This is function could be implemented in user file
