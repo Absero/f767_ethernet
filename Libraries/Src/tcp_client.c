ebeb227a0f6c8c3fcf4a2d76337c075b6c558118
Mantas A <mantas.ambraziunas@gmail.com>
2021-01-29 15:24:15 +0200

    TCP klientas dalinai pataisytas
    Po prisijungimo prie serverio jei bandoma nuskaityti duomenis (echo serveris) kai servas atsijungia procas to nebemato.

--- Libraries/Src/tcp_client.c
+++ Libraries/Src/tcp_client.c
@@ -13,11 +13,15 @@
 extern struct netif gnetif;
 
 /* Private define ------------------------------------------------------------*/
+#define TCP_CLIENT_RECONNECT_PERIOD 100	//Kas kiek laiko bandyt vel prisijungt prie serverio
+
 #define TCP_CLIENT_THREAD_PRIO  ( tskIDLE_PRIORITY + 4 )
 
 /* Private function prototypes -----------------------------------------------*/
 static void tcp_client_thread(void *arg);
 static void tcp_periodic_action(struct netconn*);
+static void tcp_echo(struct netconn *conn);
+void callbackas(struct netconn*, enum netconn_evt, u16_t len);
 
 /* Public functions ----------------------------------------------------------*/
 void tcp_client_init(void) {
@@ -27,8 +31,8 @@ void tcp_client_init(void) {
 
 /* Private functions ---------------------------------------------------------*/
 static void tcp_client_thread(void *arg) {
-	struct netconn *conn/*, *newconn*/;
-	err_t err, connect_err;
+	struct netconn *conn;
+	err_t connect_err;
 	ip_addr_t local_ip, remote_ip;
 
 	LWIP_UNUSED_ARG(arg);
@@ -38,60 +42,50 @@ static void tcp_client_thread(void *arg) {
 
 	/* Create a new connection identifier. */
 	conn = netconn_new(NETCONN_TCP);
+	conn = netconn_new_with_callback(NETCONN_TCP, callbackas);
 	if (conn != NULL) {
-		/* Bind connection to a known port number */
-		err = netconn_bind(conn, &local_ip, 55151);
-		if (err == ERR_OK) {
-			/* Tell connection to go into listening mode */
-			while (1) {
-				connect_err = netconn_connect(conn, &remote_ip, 55151);
-				/*
-				 * ERR_OK - viskas gerai, testi
-				 * ERR_ISCONN - jau prisijungta
-				 * ERR_ALREADY - galbut gali reiket (rodo kad jungiasi)
-				 */
-				if (connect_err == ERR_OK || connect_err == ERR_ISCONN) {
-					// Prisijungta, daryti veiksmus
-					tcp_periodic_action(conn);
-				} else {
-					// Perkurti sasaja is naujo, kad butu galima prisijungti
-					netconn_close(conn);
-					netconn_delete(conn);
-					conn = netconn_new(NETCONN_TCP);
-					vTaskDelay(100); // Tiesiog palaukti kad neperkurinet super daznai
-				}
+		/* Tell connection to go into listening mode */
+		while (1) {
+			connect_err = netconn_connect(conn, &remote_ip, 55151);
+			if (connect_err == ERR_OK || connect_err == ERR_ISCONN) {
+				// Prisijungta, daryti veiksmus
+//				tcp_periodic_action(conn);
+				tcp_echo(conn);
+			} else {
+				netconn_delete(conn);
+				conn = netconn_new(NETCONN_TCP);
+				vTaskDelay(TCP_CLIENT_RECONNECT_PERIOD); // Tiesiog palaukti kad neperkurinet super daznai
 			}
-		} else {
-			netconn_delete(conn);
 		}
 	}
 }
 
 static void tcp_periodic_action(struct netconn *conn) {
-	struct netbuf *buf;
 	uint16_t msg_len = 18;
 
-	buf = netbuf_new();
-	netbuf_alloc(buf, msg_len);
-	buf->p->payload = "Hello TCP world!\n\r";
-	buf->p->len = msg_len;
-
 	netconn_write(conn, "Hello TCP world!\n\r", msg_len, NETCONN_COPY);
-	netbuf_delete(buf);
-
-//					while (netconn_recv(conn, &buf) == ERR_OK) {
-//						do {
-//							netbuf_data(buf, &data, &len);
-//							netconn_write(conn, data, len, NETCONN_COPY);
-//
-//						} while (netbuf_next(buf) >= 0);
-//
-//						netbuf_delete(buf);
-//					}
-//
-//					/* Close connection and discard connection identifier. */
-//					netconn_close(conn);
 
 	vTaskDelay(1000);
 }
 
+static void tcp_echo(struct netconn *conn) {
+	struct netbuf *buf;
+	void *data;
+	u16_t len;
+	if (netconn_recv(conn, &buf) == ERR_OK) {
+		do {
+			netbuf_data(buf, &data, &len);
+			netconn_write(conn, data, len, NETCONN_COPY);
+		} while (netbuf_next(buf) >= 0);
+		netbuf_delete(buf);
+	} else {
+		// Sprendimas, bet kad perkurineja visa ta netconn gali but negerai
+		netconn_delete(conn);
+		conn = netconn_new(NETCONN_TCP);
+		vTaskDelay(10);
+	}
+}
+
+void callbackas(struct netconn *conn, enum netconn_evt event, u16_t len) {
+
+}
